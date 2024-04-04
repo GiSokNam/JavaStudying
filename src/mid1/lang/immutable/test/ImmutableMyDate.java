a1f59ef53230acb00b5ee9bcf99e053c8efacce7
GiSokNam <ngs990@naver.com>
2024-04-04 16:28:07 +0900

    불변 객체

--- src/mid1/lang/immutable/test/ImmutableMyDate.java
+++ src/mid1/lang/immutable/test/ImmutableMyDate.java
@@ -11,6 +11,9 @@ public class ImmutableMyDate {
         this.day = day;
     }
 
+    // 불변 객체에서 값을 변경하는 경우 withXxx() 처럼 with로 시작하는 경우가 많다.
+    // 정리하자면 with는 관례처럼 사용되는데, 원본 객체의 상태가 그대로 유지됨을 강조하면서 변경사항을 새 복사본에 포함하는 과정을 간결하게 표현한다.
+
     public ImmutableMyDate withYear(int newYear) {
         return new ImmutableMyDate(newYear, this.month, this.day);
     }
