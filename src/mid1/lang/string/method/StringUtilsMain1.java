19b9b20ef3fa2e45af40d5cbafe0e7210a6fac38
GiSokNam <ngs990@naver.com>
2024-04-05 21:39:02 +0900

    String 클래스

---
+++ src/mid1/lang/string/method/StringUtilsMain1.java
@@ -0,0 +1,32 @@
+package mid1.lang.string.method;
+
+public class StringUtilsMain1 {
+
+    public static void main(String[] args) {
+        int num = 100;
+        boolean bool = true;
+        Object obj = new Object();
+        String str = "Hello, Java!";
+
+        // valueOf 메서드
+        String numString = String.valueOf(num);
+        System.out.println("numString = " + numString);
+        String boolString = String.valueOf(bool);
+        System.out.println("boolString = " + boolString);
+        String objString = String.valueOf(obj);
+        System.out.println("objString = " + objString);
+
+        // 문자 + X -> 문자
+        String numString2 = "" + num;
+        System.out.println("numString2 = " + numString2);
+
+        //toCharArray 메서드
+        char[] strCharArray = str.toCharArray();
+        for (char c : strCharArray) {
+            System.out.print(c);
+        }
+        System.out.println();
+
+
+    }
+}
