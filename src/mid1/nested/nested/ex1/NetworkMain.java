bf66b250065172ea06b29381d911f96ea86f55b5
GiSokNam <ngs990@naver.com>
2024-04-09 14:53:50 +0900

    중첩 클래스, 내부 클래스 - 1

---
+++ src/mid1/nested/nested/ex1/NetworkMain.java
@@ -0,0 +1,10 @@
+package mid1.nested.nested.ex1;
+
+public class NetworkMain {
+
+    public static void main(String[] args) {
+        Network network = new Network();
+        network.sendMessage("hello java");
+
+    }
+}
